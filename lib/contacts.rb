32cd9dc521de61df6e4565299bd668f6783debd4
Taylor Mills <taylormills24@yahoo.com>
2019-11-21 00:15:34 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,8 +1,13 @@
 require 'pry'
 
-  contacts.each do |person, contact_details_hash|
-  binding.pry
-end
+ contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
   # contacts = {
   #   "Jon Snow" => {
@@ -18,12 +23,4 @@ end
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
-    end
-  end
-end
-end
+
